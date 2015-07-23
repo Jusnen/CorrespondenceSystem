93085c5e391edf148ae101d214394e02e59f4f41
Kelfi Batista Moreno <kelfi.bm@gmail.com>
2015-07-23 10:55:44 -0400

    ServiceBase (Class and Interface added)

---
+++ CorrespondenceSystem/CorrespondenceSystem/Interfaces/IServiceBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using CorrespondenceSystem.Implementations;
+
+namespace CorrespondenceSystem.Interfaces
+{
+    public interface IServiceBase
+    {
+        List<Entity<int>> GetAllEntities();
+        Entity<int> GetEntity(int id);
+        void CreateEntity(Entity<int> entity);
+        void UpdateEntity(Entity<int> entity);
+        void DeleteEntity(int id);
+    }
+}
---
+++ CorrespondenceSystem/CorrespondenceSystem/Services/ServiceBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using CorrespondenceSystem.Implementations;
+using CorrespondenceSystem.Interfaces;
+
+namespace CorrespondenceSystem.Services
+{
+    public class ServiceBase : IServiceBase
+    {
+        
+
+        public List<Entity<int>> GetAllEntities()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Entity<int> GetEntity(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CreateEntity(Entity<int> entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateEntity(Entity<int> entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteEntity(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
