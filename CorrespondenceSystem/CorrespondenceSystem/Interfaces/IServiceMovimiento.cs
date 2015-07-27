5a25d63536f8254202e9f6c39c8618f75ba892cb
Kelfi Batista Moreno <kelfi.bm@gmail.com>
2015-07-27 13:32:43 -0400

    ServiceMovimiento Added

---
+++ CorrespondenceSystem/CorrespondenceSystem/Interfaces/IServiceMovimiento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using CorrespondenceSystem.DomainClasses;
+
+namespace CorrespondenceSystem.Interfaces
+{
+    public interface IServiceMovimiento
+    {
+        void InsertMovimiento(Movimiento movimiento);
+        List<Movimiento> GetAllMovimientosFromDocumentoId(Documento documento);
+        List<Movimiento> GetAllMovimientosFromDocumentoCodigo(Documento documento);
+        List<Movimiento> GetAllMovimientosFromDepartamento(Departamento departamento);
+        List<Movimiento> GetAllMovimientosFromFecha(DateTime fechaInicio, DateTime fechaFinal);
+        Movimiento GetMovimiento(Documento documento);
+    }
+}
---
+++ CorrespondenceSystem/CorrespondenceSystem/Services/ServiceMovimiento.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using CorrespondenceSystem.DomainClasses;
+using CorrespondenceSystem.Interfaces;
+
+namespace CorrespondenceSystem.Services
+{
+    public class ServiceMovimiento : IServiceMovimiento
+    {
+        public void InsertMovimiento(Movimiento movimiento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Movimiento> GetAllMovimientosFromDocumentoId(Documento documento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Movimiento> GetAllMovimientosFromDocumentoCodigo(Documento documento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Movimiento> GetAllMovimientosFromDepartamento(Departamento departamento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Movimiento> GetAllMovimientosFromFecha(DateTime fechaInicio, DateTime fechaFinal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Movimiento GetMovimiento(Documento documento)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
