28ea3173c9053086338aee517bd2582b60219e9d
Kelfi Batista Moreno <kelfi.bm@gmail.com>
2015-07-29 17:32:19 -0400

    Documento and DocumentoCrear ViewModel added

---
+++ CorrespondenceSystem/CorrespondenceSystem/ViewModel/Documento/DocumentoCrearViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using CorrespondenceSystem.DomainClasses;
+using CorrespondenceSystem.Services;
+
+namespace CorrespondenceSystem.ViewModel.Documento
+{
+    public class DocumentoCrearViewModel
+    {
+        private const string CampoRequerido = "Campo requerido";
+        
+        public SelectList ddlRegional { get; set; }
+        public SelectList ddlDestinatarios { get; set; }
+
+        [Display(Name = "No. Oficio")]
+        [Required(ErrorMessage = CampoRequerido)]
+        public string noOficio { get; set; }
+
+        [Display(Name = "Fecha del Documento")]
+        [Required(ErrorMessage = CampoRequerido)]
+        public string fechaDocumento { get; set; }
+
+        [Display(Name = "Regional")]
+        [Required(ErrorMessage = CampoRequerido)]
+        public int regional { get; set; }
+
+        [Display(Name = "Asunto")]
+        [Required(ErrorMessage = CampoRequerido)]
+        public string asunto { get; set; }
+
+        [Display(Name = "Fecha Recibido")]
+        [Required(ErrorMessage = CampoRequerido)]
+        public string fechaRecibido { get; set; }
+
+        [Display(Name = "Destinatarios")]
+        [Required(ErrorMessage = CampoRequerido)]
+        public int idDepartamento { get; set; }
+
+    }
+}
---
+++ CorrespondenceSystem/CorrespondenceSystem/ViewModel/Documento/DocumentoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CorrespondenceSystem.DomainClasses;
+
+namespace CorrespondenceSystem.ViewModel.Documento
+{
+    public class DocumentoViewModel
+    {
+        public string codigo { get; set; }
+        public DateTime fechaCreacionUsuario { get; set; }
+        public DateTime fechaRegistroUsuario { get; set; }
+        public string asunto { get; set; }
+        public Departamento departamento { get; set; }
+        public DateTime fechaCreacion { get; set; }
+        public DateTime? fechaModificacion { get; set; }
+        public int usuarioCreacion { get; set; }
+        public int usuarioModificacion { get; set; }
+        
+    }
+}
