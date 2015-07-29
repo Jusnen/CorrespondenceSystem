e7edf2bd3e8812d2df6758e9d9ee7ef5c125ece6
Kelfi Batista Moreno <kelfi.bm@gmail.com>
2015-07-29 17:31:50 -0400

    ServiceDepartamento Added

---
+++ CorrespondenceSystem/CorrespondenceSystem/Interfaces/IServiceDepartamento.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+using CorrespondenceSystem.DomainClasses;
+
+namespace CorrespondenceSystem.Interfaces
+{
+    public interface IServiceDepartamento
+    {
+        List<Departamento> GetAllDepartamentos();
+        List<Departamento> GetAllDepartamentosByTipoDepartamento(int idTipoDepartamento);
+    }
+}
---
+++ CorrespondenceSystem/CorrespondenceSystem/Services/ServiceDepartamento.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CorrespondenceSystem.DomainClasses;
+using CorrespondenceSystem.Interfaces;
+using CorrespondenceSystem.Repositories;
+using MvcGCP.NHibernateHelpers;
+using NHibernate;
+
+namespace CorrespondenceSystem.Services
+{
+    public class ServiceDepartamento : IServiceDepartamento
+    {
+        private static NHibernateHelper nHibernateHelper;
+        private static ISessionFactory _sessionFactory;
+        private static ISession _session;
+        private readonly IRepository<Departamento, int> _repository;
+
+        public ServiceDepartamento()
+        {
+            nHibernateHelper = new NHibernateHelper();
+            _sessionFactory = nHibernateHelper.CreateSessionFactory();
+            _session = _sessionFactory.OpenSession();
+            _repository = new RepositoryBase<Departamento, int>(_session);
+        }
+        public List<Departamento> GetAllDepartamentos()
+        {
+            return _repository.GetAll().ToList();
+        }
+
+        public List<Departamento> GetAllDepartamentosByTipoDepartamento(int idTipoDepartamento)
+        {
+            return _repository.GetAll(x => x.tipoDepartamento.id == idTipoDepartamento).ToList();
+        }
+    }
+}
