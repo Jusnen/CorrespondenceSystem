e038f2a4b1007e80ccae87136d68431c27b6233c
Kelfi Batista Moreno <kelfi.bm@gmail.com>
2015-07-30 17:05:21 -0400

    Boostrap select added

---
+++ CorrespondenceSystem/CorrespondenceSystem/App_Script/Documento/JqueryRegistrarNuevo.js
@@ -0,0 +1,25 @@
+﻿
+$(document).on("click", "#hideAlert", function() {
+
+    $("#AlertSuccess").hide();
+
+});
+
+
+function onBeginFunction(parameters)
+{
+
+}
+
+function OnSuccessFunction(jsonResult)
+{
+    $("#AlertSuccess").html("");
+    $("#AlertSuccess").html("<span>" + jsonResult.Message + " </span> <a id='hideAlert' href='#' class='alert-link' style='float:right'>x</a>");
+    $("#AlertSuccess").show();
+    $("#DocumentoRegistrarNuevo").find('form')[0].reset();
+}
+
+function OnFailureFunction(jsonResult) {
+
+    
+}
